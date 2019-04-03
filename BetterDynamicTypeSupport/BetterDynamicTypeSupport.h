671c022928e63760766c53a87dd2acb89a98d7a6
Bart van Kuik <bart@vankuik.nl>
2019-04-03 12:24:25 +0200

    Initial Commit

---
+++ BetterDynamicTypeSupport/BetterDynamicTypeSupport.h
@@ -0,0 +1,19 @@
+//
+//  BetterDynamicTypeSupport.h
+//  BetterDynamicTypeSupport
+//
+//  Created by Bart van Kuik on 03/04/2019.
+//  Copyright © 2019 DutchVirtual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BetterDynamicTypeSupport.
+FOUNDATION_EXPORT double BetterDynamicTypeSupportVersionNumber;
+
+//! Project version string for BetterDynamicTypeSupport.
+FOUNDATION_EXPORT const unsigned char BetterDynamicTypeSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterDynamicTypeSupport/PublicHeader.h>
+
+
