de71be6414e90c4231202c4d87ca74cf11ed9883
Bart van Kuik <bart@vankuik.nl>
2019-04-06 22:15:00 +0200

    Added textfield

---
+++ BetterDynamicTypeSupport/BDTSTextField.swift
@@ -0,0 +1,9 @@
+//
+//  BDTSTextField.swift
+//  BetterDynamicTypeSupport
+//
+//  Created by Bart van Kuik on 06/04/2019.
+//  Copyright © 2019 DutchVirtual. All rights reserved.
+//
+
+import Foundation
